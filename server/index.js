626b3ddb61fcbc952be03da5d0f30da8bfca7636
priya kumari <priya1kumari22003@gmail.com>
2023-10-17 11:50:46 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -8,7 +8,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin:[""],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', router);
