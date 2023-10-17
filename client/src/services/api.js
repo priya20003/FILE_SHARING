7915d28df6928167356227183af7841bdea1ffcf
priya kumari <priya1kumari22003@gmail.com>
2023-10-17 12:13:21 +0530

    changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-const API_URI = 'http://localhost:8000';
+const API_URI = 'https://file-sharing-api.vercel.app/';
+
 
 export const uploadFile = async (data) => {
-    try {
+    try { 
+        axios.defaults.withCredentials=true;
         const response = await axios.post(`${API_URI}/upload`, data);
         return response.data;
     } catch (error) {
